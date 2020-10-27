2e88647bfabf6fdd4e0b2f3ea6d62b4c8bf4f19d
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-27 13:33:23 +0700

    Day1 JsBasics PigGame Start

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const score1 = document.getElementById('score--0');
+const score2 = document.getElementById('score--1');
+const dice = document.querySelector('.dice')
+
+score1.textContent =0;
+score2.textContent =0;
+dice.style.display ='none'
